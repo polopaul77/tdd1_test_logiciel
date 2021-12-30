77ce8b617aee39bac6d50f7f4ba8513fe9525290
polopaul77 <paul.fourcade1@gmail.com>
2021-12-30 20:28:55 +0100

    Ajout du test de la fonction moyenne

--- tests_funcs.py
+++ tests_funcs.py
@@ -16,5 +16,12 @@ class TestFuncs(unittest.TestCase):
 		self.assertEqual(funcs.min_int(3,3),3)
 		self.assertEqual(funcs.min_int(-5,-5),-5)
 
+	def test_avg_int(self):
+		self.assertEqual(funcs.avg_int([1,2,3,4,5]),3)
+		self.assertEqual(funcs.avg_int([-2,-4]),-3)
+		self.assertEqual(funcs.avg_int([-2,-1,1,2]),0)
+		self.assertEqual(funcs.avg_int([0,0,0,0,0]),0)
+		self.assertEqual(funcs.avg_int([13]),13)
+
 if __name__ == '__main__':
 	unittest.main()
