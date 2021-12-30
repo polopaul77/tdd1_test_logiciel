5b97544b80aa61085f88fcce23d0464b408f003b
polopaul77 <paul.fourcade1@gmail.com>
2021-12-30 20:19:46 +0100

    Ajout du test de la fonction minimun

--- tests_funcs.py
+++ tests_funcs.py
@@ -9,5 +9,12 @@ class TestFuncs(unittest.TestCase):
 		self.assertEqual(funcs.max_int(-1,2),2)
 		self.assertEqual(funcs.max_int(0,0),0)
 
+	def test_min_int(self):
+		self.assertEqual(funcs.min_int(0,4),0)
+		self.assertEqual(funcs.min_int(-1,-7),-7)
+		self.assertEqual(funcs.min_int(-5,5),-5)
+		self.assertEqual(funcs.min_int(3,3),3)
+		self.assertEqual(funcs.min_int(-5,-5),-5)
+
 if __name__ == '__main__':
 	unittest.main()
