7ad26f6d9dce65851babe67b22580feac116b846
Modar Nasser <modar1999@gmail.com>
2021-12-31 14:34:05 +0100

    Ajout test de la fonction is_username_valid

--- funcs.py
+++ funcs.py
@@ -3,3 +3,6 @@ def max_int(a,b):
 		return b
 	else :
 		return a
+
+def is_username_valid(username: str) -> bool:
+	return None
--- tests_funcs.py
+++ tests_funcs.py
@@ -9,5 +9,14 @@ class TestFuncs(unittest.TestCase):
 		self.assertEqual(funcs.max_int(-1,2),2)
 		self.assertEqual(funcs.max_int(0,0),0)
 
+	def test_is_username_valid(self):
+		self.assertEqual(funcs.is_username_valid("abc"), True)
+		self.assertEqual(funcs.is_username_valid("XYZ"), True)
+		self.assertEqual(funcs.is_username_valid("Abc123"), True)
+		self.assertEqual(funcs.is_username_valid("a"), False)
+		self.assertEqual(funcs.is_username_valid("Abc-#"), False)
+		self.assertEqual(funcs.is_username_valid("HtmlGoes<br>"), False)
+		self.assertEqual(funcs.is_username_valid("\u00C8re"), False)  # \u00C8 = È
+
 if __name__ == '__main__':
 	unittest.main()
