d6b933e4dc7cc0c5934e280e8568c663617e9e47
Modar Nasser <modar1999@gmail.com>
2022-01-01 14:26:07 +0100

    Implémentation de is_password_valid

--- funcs.py
+++ funcs.py
@@ -8,13 +8,15 @@ def max_int(a,b):
 
 
 username_regex = re.compile("([A-Za-z0-9]){4,}")
+password_regex = re.compile("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,}")
 
 def is_username_valid(username: str) -> bool:
 	match = username_regex.match(username)
 	return match is not None and match.group() == username
 
 def is_password_valid(password: str) -> bool:
-	return None
+	match = password_regex.match(password)
+	return match is not None and match.group() == password
 
 def user_login(cursor, username: str, password: str) -> bool:
 	return None
